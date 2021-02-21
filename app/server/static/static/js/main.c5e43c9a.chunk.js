6e48f44bb9a73fdda83071df6c5ab39e54f2a206
Arieski <zhangzijinning@hotmail.com>
2021-02-20 20:49:24 -0500

    clean up build folder inside server folder

--- app/server/static/static/js/main.c5e43c9a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),i=n(3),o=n.n(i),s=(n(8),n(0));var l=function(){return Object(s.jsx)("h1",{children:"Hello World!"})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(l,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.c5e43c9a.chunk.js.map
