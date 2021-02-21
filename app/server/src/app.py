418395eef476861aca6b8a834e647eb46e67fd38
Arieski <zhangzijinning@hotmail.com>
2021-02-20 20:41:02 -0500

    update static folder path

--- app/server/src/app.py
+++ app/server/src/app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 import os
 from typing import Dict
 
-app = Flask(__name__, static_url_path='')
-# app = Flask(__name__, static_folder="../static")
+# app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='', static_folder="../../client/build/")
 
 DirPATH = os.path.abspath(os.path.dirname(__name__))
 
