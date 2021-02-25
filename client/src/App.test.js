bc3aec0d7a7baf9bb3873a1827ebac89f4b35728
kexinlin <kexin.lin@mail.utoronto.ca>
2021-02-25 18:41:44 +0800

    initialize react unit test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("should show a header bar", () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText("Moguls Analysis");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("should show a table title", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Saved Data");
+  expect(linkElement).toBeInTheDocument();
 });
