c544acdce9405dccfd72e7956b50cf10cfe1a2d7
Arieski <zhangzijinning@hotmail.com>
2021-02-25 05:02:58 -0500

    add unittest files for database operation and server

---
+++ unittest/test_db.py
@@ -0,0 +1,19 @@
+import unittest
+
+from .. import app
+
+
+class DatabaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_add(self):
+        self.assertIsNotNone('Success!')
+
+
+if __name__ == '__main__':
+    unittest.main()
