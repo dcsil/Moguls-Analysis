3aaa5557e6c22ced94ff58510b5ba439ff465ca6
Arieski <zhangzijinning@hotmail.com>
2021-03-17 01:44:42 -0400

    add http request to upload video

--- client/src/actions/Requests.js
+++ client/src/actions/Requests.js
@@ -1,7 +1,35 @@
-export const getAllDate = (data) => {
+/** Call this function to send video file to server
+ * 
+ * @param {*} video the video file that is needed to be upload
+ */
+export const uploadVideo = video => {
+    const request = new Request("/videoUpload", {
+        method: "POST",
+        headers: {
+            Accept: "application/json, video/*, */*",
+            "Content-Type": "video/*"
+        },
+        body: video
+    });
+
+    // Send the request with fetch()
+    fetch(request)
+        .then(res => {
+            if (res.status === 200) {
+                console.log(res.json());    // for dev and debug
+                return res.json();
+            } else {
+                alert("Sorry, fail to upload video, please try again");
+            }
+        }).catch((error) => {
+            console.log("ERROR: "  + error)
+        })
+}
+
+/** Call this function to get all saved data from database */
+export const getAllDate = () => {
     const request = new Request("/getAllDate", {
         method: "GET",
-        body: JSON.stringify(data),
         headers: {
             Accept: "application/json, text/plain, */*",
             "Content-Type": "application/json"
@@ -22,7 +50,10 @@ export const getAllDate = (data) => {
         })
 }
 
-
+/** Call this function to send the data to database and save it in database
+ * 
+ * @param {*} data the data file that is needed to be saved
+ */
 export const saveData = data => {
     const request = new Request("/addData", {
         method: "POST",
