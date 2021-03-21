020487d79be9d8f244f45b29a07a43ebb6635609
kexinlin <kexin.lin@mail.utoronto.ca>
2021-03-21 10:27:05 +0800

    add initial auth page (need to further implement)

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "./Header";
 import Analyzer from "./Analyzer";
+import Auth from "./Auth";
+import Context from "../utils/context";
 import LinearProgress from "@material-ui/core/LinearProgress";
 
 function Main() {
-  return (
-    <div>
-      {/* <LinearProgress /> */}
-      <Header />
-      <Analyzer />
-    </div>
-  );
+  const context = useContext(Context);
+
+  let content = <Auth />;
+
+  if (context.isAuth) {
+    content = (
+      <div>
+        {/* <LinearProgress /> */}
+        <Header />
+        <Analyzer />
+      </div>
+    );
+  }
+
+  return content;
 }
 
 export default Main;
